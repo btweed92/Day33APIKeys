62cad7cea2b1f780c2d30c8acb65621beb0d139e
Brandyn Tweedly <tweedly92@gmail.com>
2019-08-28 15:24:22 -0400

    API key get ignore

---
+++ Day33WebAPIDay3/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Day33WebAPIDay3.Models;
+using System.Net.Http;
+
+namespace Day33WebAPIDay3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var catBreeds = CatAPIMethods.CatBreeds().Result;
+            return View(catBreeds);
+        }
+
+
+        public IActionResult RandomCatByBreed()
+        {
+            var catImages = CatAPIMethods.GetBreedImages().Result;
+            return View(catImages);
+        }
+
+
+
+
+
+
+    }
+}
